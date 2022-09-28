01ee1be08d735f82778b6868449f890e16f3fa89
willycede <willycede.ucsg@gmail.com>
2022-09-28 00:11:28 -0500

    changes in colors

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -6,8 +6,8 @@ export default {
 	copyrightText: '© All Rights Reversed | Made With Love by IRON NETWORK for better Web',     // Copyright Text
 	//theme color
 	color: {
-      primary: '#0000A0',
-      accent : '#00FFFF',
+      primary: '#00006B',
+      accent : '#0068FF',
 	},
 	apiUrl: process.env.NODE_ENV == 'production' ? 'http://localhost:3500' :  'http://localhost:3500',
 	algoliaApiKey: '6be0576ff61c053d5f9a3225e2a90f76'
