89e063ae69ea03975295cb0971b698f4b7930b7b
willycede <44324049+willycede@users.noreply.github.com>
2024-08-10 13:59:28 -0500

    ADD env

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -10,7 +10,7 @@ export default {
       primary: '#00006B',
       accent : '#0068FF',
 	},
-	apiUrl: process.env.VUE_APP_NODE_ENV == 'production' ? 'http://3.14.140.168:3500' :  'http://localhost:3500',
+	apiUrl: process.env.VUE_APP_NODE_ENV == 'production' ? process.env.VUE_APP_API :  'http://localhost:3500',
 	algoliaApiKey: '6be0576ff61c053d5f9a3225e2a90f76',
 
 	//configuracion correo
